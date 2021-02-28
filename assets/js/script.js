2b9200e9802840333b653570cf0460863bec957f
Elise Daly <elise.samantha.daly@gmail.com>
2021-02-28 17:13:48 -0500

    Completed js, comments, flavortext. CSS is next

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var highscoreButton = document.querySelector("#highscoreButton");
 var timer = document.querySelector("#timer");
 var timeLeft;
 var timeInterval;
+var quizHeader = document.querySelector("#quizHeader");
+var instructions = document.querySelector("#instructions");
 var startButton = document.querySelector("#startButton");
 var questionNumber;
 var score;
@@ -13,10 +15,12 @@ var b = document.querySelector("#b");
 var c = document.querySelector("#c");
 var d = document.querySelector("#d");
 
+var highscoresHeader = document.querySelector("#highscoresHeader");
 var highscoreList = document.querySelector("#highscoreList");
 var highscores = [];
 
 var initialsForm = document.querySelector("#initialsForm");
+var scoreDisplay = document.querySelector("#scoreDisplay");
 var initialsInput = document.querySelector("#initialsInput");
 
 var backButton = document.querySelector("#backButton");
@@ -27,7 +31,9 @@ var rightOrWrong = document.querySelector("#rightOrWrong");
 //FUNCTIONS
 //Triggered by clicking Start button
 function startGame() {
-  //Hide Start button, show question and choices
+  //Hide intro text/Start button, show question and choices
+  quizHeader.style.display = "none";
+  instructions.style.display = "none";
   startButton.style.display = "none";
   question.style.display = "block";
   a.style.display = "block";
@@ -68,39 +74,39 @@ function nextQuestion() {
 
   switch (questionNumber) {
     case 1:
-      question.innerHTML = "Question1";
-      a.innerHTML = "a1";
-      b.innerHTML = "b1";
-      c.innerHTML = "c1";
-      d.innerHTML = "d1";
+      question.innerHTML = "1. Which of these grows on trees?";
+      a.innerHTML = "a) Apples"; //Correct
+      b.innerHTML = "b) Potatoes";
+      c.innerHTML = "c) Riddles";
+      d.innerHTML = "d) Money";
       break;
     case 2:
-      question.innerHTML = "Question2";
-      a.innerHTML = "a2";
-      b.innerHTML = "b2";
-      c.innerHTML = "c2";
-      d.innerHTML = "d2";
+      question.innerHTML = "2. Bats are...?";
+      a.innerHTML = "a) Birds";
+      b.innerHTML = "b) Mammals"; //Correct
+      c.innerHTML = "c) Marsupials";
+      d.innerHTML = "d) Giant bugs";
       break;
     case 3:
-      question.innerHTML = "Question3";
-      a.innerHTML = "a3";
-      b.innerHTML = "b3";
-      c.innerHTML = "c3";
-      d.innerHTML = "d3";
+      question.innerHTML = "3. Which of these is not a dinosaur genus?";
+      a.innerHTML = "a) Stegosaurus";
+      b.innerHTML = "b) Diplodocus";
+      c.innerHTML = "c) Smilodon"; //Correct
+      d.innerHTML = "d) Tyrannosaurus";
       break;
     case 4:
-      question.innerHTML = "Question4";
-      a.innerHTML = "a4";
-      b.innerHTML = "b4";
-      c.innerHTML = "c4";
-      d.innerHTML = "d4";
+      question.innerHTML = "4. Which fact is true about <i>Musa acuminata</i>, the Cavendish banana?";
+      a.innerHTML = "a) It was replaced by the Gros Michel cultivar after being devastated by Panama disease in the 1950s";
+      b.innerHTML = "b) It was the first kind of banana to be introduced to Europe";
+      c.innerHTML = "c) Individual plants are called trees";
+      d.innerHTML = "d) It was named after William Cavendish, 6th Duke of Devonshire"; //Correct
       break;
     case 5:
-      question.innerHTML = "Question5";
-      a.innerHTML = "a5";
-      b.innerHTML = "b5";
-      c.innerHTML = "c5";
-      d.innerHTML = "d5";
+      question.innerHTML = "5. Did you have fun with this quiz?";
+      a.innerHTML = "a) Yes!"; //Correct
+      b.innerHTML = "b) No.";
+      c.innerHTML = "c) Really, I did not.";
+      d.innerHTML = "d) Please stop asking!";
       break;
   }
 }
@@ -111,12 +117,12 @@ function selectAnswer(event) {
   if (questionNumber === 1) {
     //If selected choice is correct, tell user and increase their score
     if (event.target.classList.contains("q1Right")) {
-      rightOrWrong.innerHTML = "You got Question 1 right!";
+      rightOrWrong.innerHTML = "Right!";
       score++;
     }
     //If selected choice is incorrect, tell user and subtract time from timer
     else {
-      rightOrWrong.innerHTML = "You got Question 1 wrong!";
+      rightOrWrong.innerHTML = "Wrong!";
       timeLeft-=15;
     }
     //Proceed to the next question
@@ -125,11 +131,11 @@ function selectAnswer(event) {
   //Question 2
   else if (questionNumber === 2) {
     if (event.target.classList.contains("q2Right")) {
-      rightOrWrong.innerHTML = "You got Question 2 right!";
+      rightOrWrong.innerHTML = "Right!";
       score++;
     }
     else {
-      rightOrWrong.innerHTML = "You got Question 2 wrong!";
+      rightOrWrong.innerHTML = "Wrong!";
       timeLeft-=15;
     }
     nextQuestion();
@@ -137,11 +143,11 @@ function selectAnswer(event) {
   //Question 3
   else if (questionNumber === 3) {
     if (event.target.classList.contains("q3Right")) {
-      rightOrWrong.innerHTML = "You got Question 3 right!";
+      rightOrWrong.innerHTML = "Right!";
       score++;
     }
     else {
-      rightOrWrong.innerHTML = "You got Question 3 wrong!";
+      rightOrWrong.innerHTML = "Wrong!";
       timeLeft-=15;
     }
     nextQuestion();
@@ -149,11 +155,11 @@ function selectAnswer(event) {
   //Question 4
   else if (questionNumber === 4) {
     if (event.target.classList.contains("q4Right")) {
-      rightOrWrong.innerHTML = "You got Question 4 right!";
+      rightOrWrong.innerHTML = "Right!";
       score++;
     }
     else {
-      rightOrWrong.innerHTML = "You got Question 4 wrong!";
+      rightOrWrong.innerHTML = "Wrong!";
       timeLeft-=15;
     }
     nextQuestion();
@@ -161,11 +167,11 @@ function selectAnswer(event) {
   //Question 5
   else if (questionNumber === 5) {
     if (event.target.classList.contains("q5Right")) {
-      rightOrWrong.innerHTML = "You got Question 5 right!";
+      rightOrWrong.innerHTML = "Right!";
       score++;
     }
     else {
-      rightOrWrong.innerHTML = "You got Question 5 wrong!";
+      rightOrWrong.innerHTML = "Wrong!";
       timeLeft-=15;
     }
     //This is the last question; display highscore list/initials form. More questions can be inserted into this function if desired
@@ -175,17 +181,21 @@ function selectAnswer(event) {
 
 //Trigged by clicking Highscore button, answering the last question, or running out of time
 function showHighscores() {
-  //Hide Highscore/Start buttons and questions/answer choices
+  //Hide Highscore/Start buttons/intro text and questions/answer choices
   highscoreButton.style.display = "none";
+  quizHeader.style.display = "none";
+  instructions.style.display = "none";
   startButton.style.display = "none";
   question.style.display = "none";
   a.style.display = "none";
   b.style.display = "none";
   c.style.display = "none";
   d.style.display = "none";
-  //Show highscore list, initials form, Back/Clear buttons
+  //Show highscore header/list, initials form, Back/Clear buttons
+  highscoresHeader.style.display = "block";
   highscoreList.style.display = "block";
   initialsForm.style.display = "block";
+  scoreDisplay.innerHTML = "All done! Your score is " + score + "/5."
   backButton.style.display = "block";
   clearButton.style.display = "block";
 
@@ -263,12 +273,15 @@ initialsForm.addEventListener("submit", function(event) {
 
 //Click to start the game over
 backButton.addEventListener("click", function() {
-  //Show Highscore/Start buttons, reset timer
+  //Show Highscore/Start buttons/intro text, reset timer
   highscoreButton.style.display = "block";
   timeLeft = 0;
   timer.innerHTML = timeLeft;
+  quizHeader.style.display = "block";
+  instructions.style.display = "block";
   startButton.style.display = "block";
-  //Hide highscore list/initials form/Clear and Back buttons, empty out right/wrong indicator
+  //Hide highscore header, list/initials form/Clear and Back buttons, empty out right/wrong indicator
+  highscoresHeader.style.display = "none";
   highscoreList.style.display = "none";
   initialsForm.style.display = "none";
   backButton.style.display = "none";
